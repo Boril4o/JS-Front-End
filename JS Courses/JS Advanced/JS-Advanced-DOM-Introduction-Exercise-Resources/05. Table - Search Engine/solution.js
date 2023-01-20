36a8063198371e6b4a9e3bd3e49366d502650bf9
Boris <borisgeorgievdg@gmail.com>
2023-01-20 16:50:03 +0200

    DOM Exercises are done

--- JS Courses/JS Advanced/JS-Advanced-DOM-Introduction-Exercise-Resources/05. Table - Search Engine/solution.js
+++ JS Courses/JS Advanced/JS-Advanced-DOM-Introduction-Exercise-Resources/05. Table - Search Engine/solution.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let items = Array.from(document.querySelectorAll('tbody > tr'));
+      let input = document.getElementById('searchField');
 
+      let regex = new RegExp(input.value);
+         items.map(e => {
+            e.classList.remove('select');
+            if (e.innerHTML.match(regex) !== null) {
+                e.className = 'select';
+            }
+        });
+        searchField.value = '';
    }
 }
--- JS Courses/JS Advanced/JS-Advanced-DOM-Introduction-Exercise-Resources/06. Format the Text/solution.js
+++ JS Courses/JS Advanced/JS-Advanced-DOM-Introduction-Exercise-Resources/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+    let textArea = document.getElementById('input');
+    let output = document.getElementById('output');
+
+    let sentences = Array.from(textArea.value.split('.').filter(x => x.length > 0));
+    for (let i = 0; i < sentences.length; i += 3) {
+        let result = [];
+        for (let j = 0; j < 3; j++) {
+          if(sentences[i + j]) {
+            result.push(sentences[i + j]);
+          }
+        }
+        let paragraph = result.join('. ') + '.';
+        output.innerHTML += `<p>${paragraph}</p>`;
+    }
 }
