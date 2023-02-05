218d59a34e19a6ed791124b8c551d687a0d65568
Boris <borisgeorgievdg@gmail.com>
2023-02-05 17:24:52 +0200

    useEffect hook is learnt

--- JS Courses/React/test/src/index.js
+++ JS Courses/React/test/src/index.js
@@ -1,5 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function Button() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    React.useEffect(() => {
+        console.log('Count changed to: ' + count);  
+    }, [count]);
+    
+    return (
+        <>
+            <h1>Count:{count}</h1>
+            <button onClick={handleClick}>Add</button>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello Wolrd!</h1>);
+root.render(<Button/>);
